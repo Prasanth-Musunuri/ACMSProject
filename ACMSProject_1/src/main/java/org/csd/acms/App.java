3097774da9eda104a25d4963bd4e695b54551f20
Prasanth Musunuri <mvs.prasanth@gmail.com>
2015-04-01 18:06:02 +0530

    Second Commit

---
+++ ACMSProject_1/src/main/java/org/csd/acms/App.java
@@ -0,0 +1,13 @@
+package org.csd.acms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
